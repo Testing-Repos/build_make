2775507060feb9d1800dee4bfebd0fcb15c028ef
xplodwild <xplod@ouverta.fr>
2013-08-24 17:40:37 +0100

    build: Add bacon target, and mka alias

--- envsetup.sh
+++ envsetup.sh
@@ -1616,6 +1616,7 @@ function mka() {
             ;;
         *)
             schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
+
             ;;
     esac
 }
